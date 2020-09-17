ce1d75bcc514ff12c827f17373b7c7bf3b88439a
Ghardlotte <g_hardlotte@hotmail.com>
2020-09-16 22:34:46 -0600

    added console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a messgae");
+console.log(10 + 10);
+console.log("Out robot's name is " + playerName);
+function fight() {
+window.alert("The Fight had begun");
+}
+//fight()
